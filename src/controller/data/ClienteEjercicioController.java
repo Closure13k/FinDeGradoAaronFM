d77fc76d0f218646353f0d0f56d185125f6ab7df
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-08 12:51:55 +0200

    Update ClienteEjercicioController.java

--- src/controller/data/ClienteEjercicioController.java
+++ src/controller/data/ClienteEjercicioController.java
@@ -86,8 +86,8 @@ public class ClienteEjercicioController {
         try (var ps = dbCon.prepareStatement(ClienteEjercicioEntity.selectQuery());
              var rs = ps.executeQuery()) {
             List<ClienteEjercicio> listado = new ArrayList<>();
-            if(rs.next()){
-                do{
+            if (rs.next()) {
+                do {
                     var ejercicio = new Ejercicio();
                     ejercicio.setIdEjercicio(rs.getInt(ClienteEjercicioEntity.ID_EJERCICIO));
                     ejercicio.setTipo(rs.getString(EjercicioEntity.TIPO));
@@ -118,18 +118,72 @@ public class ClienteEjercicioController {
         Connection dbCon = DatabaseConnection.getInstance().getConnection();
         try (PreparedStatement ps = dbCon.prepareStatement(ClienteEjercicioEntity.insertQuery())) {
 
-            ps.setInt(1, clienteEjercicio.getCliente().getIdCliente());
-            ps.setInt(2, clienteEjercicio.getEjercicio().getIdEjercicio());
-            ps.setDate(3, clienteEjercicio.getFecha());
-            ps.setInt(4, clienteEjercicio.getSeries());
-            ps.setFloat(5, clienteEjercicio.getPeso());
-            ps.setString(6, clienteEjercicio.getComentario());
+            prepareInsert(clienteEjercicio, ps);
 
             ps.executeUpdate();
             return clienteEjercicio;
         } catch (SQLException e) {
             throw new RuntimeException(readSQLException(e));
         }
+    }
+
+    public ClienteEjercicio updateClienteEjercicio(ClienteEjercicio clienteEjercicio) {
+        Connection dbCon = DatabaseConnection.getInstance().getConnection();
+        try (PreparedStatement ps = dbCon.prepareStatement(ClienteEjercicioEntity.updateQuery())) {
+            prepareUpdate(clienteEjercicio, ps);
+
+            ps.executeUpdate();
+            return clienteEjercicio;
+        } catch (SQLException e) {
+            throw new RuntimeException(readSQLException(e));
+        }
+    }
+
+    public ClienteEjercicio deleteClienteEjercicio(ClienteEjercicio clienteEjercicio) {
+        Connection dbCon = DatabaseConnection.getInstance().getConnection();
+        try (PreparedStatement ps = dbCon.prepareStatement(ClienteEjercicioEntity.deleteQuery())) {
+            ps.setInt(1, clienteEjercicio.getCliente().getIdCliente());
+            ps.setInt(2, clienteEjercicio.getEjercicio().getIdEjercicio());
+            ps.setDate(3, clienteEjercicio.getFecha());
+
+            ps.executeUpdate();
+            return clienteEjercicio;
+        } catch (SQLException e) {
+            throw new RuntimeException(readSQLException(e));
+        }
+    }
+
+
+    /**
+     * Prepara el statement para insertar.
+     *
+     * @param clienteEjercicio el cliente_ejercicio a insertar.
+     * @param ps               el PreparedStatement.
+     * @throws SQLException
+     */
+    private void prepareInsert(ClienteEjercicio clienteEjercicio, PreparedStatement ps) throws SQLException {
+        ps.setInt(1, clienteEjercicio.getCliente().getIdCliente());
+        ps.setInt(2, clienteEjercicio.getEjercicio().getIdEjercicio());
+        ps.setDate(3, clienteEjercicio.getFecha());
+        ps.setInt(4, clienteEjercicio.getSeries());
+        ps.setFloat(5, clienteEjercicio.getPeso());
+        ps.setString(6, clienteEjercicio.getComentario());
+    }
+
+    /**
+     * Prepara el statement para actualizar.
+     *
+     * @param clienteEjercicio el cliente_ejercicio a actualizar.
+     * @param ps               el PreparedStatement.
+     * @throws SQLException
+     */
+    private static void prepareUpdate(ClienteEjercicio clienteEjercicio, PreparedStatement ps) throws SQLException {
+        ps.setInt(1, clienteEjercicio.getSeries());
+        ps.setFloat(2, clienteEjercicio.getPeso());
+        ps.setString(3, clienteEjercicio.getComentario());
+        ps.setInt(4, clienteEjercicio.getCliente().getIdCliente());
+        ps.setInt(5, clienteEjercicio.getEjercicio().getIdEjercicio());
+        ps.setDate(6, clienteEjercicio.getFecha());
     }
 
 }
