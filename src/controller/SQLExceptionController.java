8868fa873e9dfa3d0256d4b9770b341003e866ef
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-08 08:04:46 +0200

    Update SQLExceptionController.java

--- src/controller/SQLExceptionController.java
+++ src/controller/SQLExceptionController.java
@@ -5,13 +5,22 @@ import java.sql.SQLException;
 public class SQLExceptionController {
 
     public static String readSQLException(SQLException sqle) {
-        System.out.println(sqle.getErrorCode() + ": " + sqle.getMessage());
+        final String message = sqle.getMessage();
+        if (message.contains("link failure")) {
+            return """
+                   Error al conectarse contra la base de datos.
+                   Es posible que la IP no esté bien configurada o que el servidor no está disponible.""";
+        }
+
+        //TODO: Remove debugging checks in production.
+        final int errorCode = sqle.getErrorCode();
+        System.out.println(errorCode + ": " + message);
         StringBuilder sb = new StringBuilder();
-        switch (sqle.getErrorCode()) {
+        switch (errorCode) {
             case 1406 ->
                 sb.append("Valor supera la capacidad del tipo de datos.\n");
             case 1048 ->
-                sb.append("Hay una o varias columnas que no admiten valores nulos.\n");
+                sb.append("Hay una o varias columnas que no admiten valores sin dato.\n");
             case 1062 ->
                 sb.append("Se está usando un valor que ya existe en la tabla.\n");
             default ->
@@ -21,9 +30,9 @@ public class SQLExceptionController {
         return sb
                 .append(sqle.getClass().getSimpleName()).append("  -  ") //TODO: Remove debug marks.
                 .append("Información detallada:\n")
-                .append(sqle.getErrorCode())
+                .append(errorCode)
                 .append(": ")
-                .append(sqle.getMessage())
+                .append(message)
                 .toString();
 
     }
