4ba307d200789b6de0fb8bb30b990ae66bdca87e
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-09 10:23:27 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,71 +1,30 @@
 package main;
 
 import controller.data.ClienteController;
+import controller.exception.FTPControllerException;
 import controller.exception.SQLExceptionController;
 import controller.database.DatabaseController;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import controller.ftp.FTPController;
 import model.Cliente;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        /*
-        System.out
-                .printf("Sentencias Ejercicios.")
-                .printf(EjercicioEntity.insertQuery())
-                .printf("\n")
-                .printf(EjercicioEntity.selectQuery())
-                .printf("\n")
-                .printf(EjercicioEntity.selectQuery().concat(EjercicioEntity.whereId()))
-                .printf("\n")
-                .printf(EjercicioEntity.deleteQuery())
-                .printf("\n")
-                .printf(EjercicioEntity.updateQuery())
-                .printf("\n")
-                .printf("---------------------------------------------------------")
-                .printf("\n");
-*/
-        try {
-            ejecutaBackendPruebas();
-        } catch (RuntimeException rex) {
-            System.err.println(rex.getMessage());
-        }
-        //ejecutaBackendPruebas();
     }
 
-    private static void ejecutaBackendPruebas() {
-        Connection con = DatabaseController.getInstance().getConnection();
-        try {
-            con.setAutoCommit(false);
-
-     
-
-
-
-
-
-
-            con.commit();
-            con.setAutoCommit(true);
-        } catch (SQLException rex) {
-            System.err.println(SQLExceptionController.readSQLException(rex));
-            try {
-                con.rollback();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                System.err.println(SQLExceptionController.readSQLException(ex));
-            }
-        }
-        try {
-            con.setAutoCommit(true);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.err.println(SQLExceptionController.readSQLException(ex));
-        }
-    }
 }
