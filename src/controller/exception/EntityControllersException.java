e7116d8bbb6d21dee341e297e4e04fad3889eec4
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-15 13:53:17 +0200

    Update EntityControllersException.java

--- src/controller/exception/EntityControllersException.java
+++ src/controller/exception/EntityControllersException.java
@@ -2,7 +2,16 @@ package controller.exception;
 
 import java.sql.SQLException;
 
+/**
+ * Identificador personalizado para SQLExceptions.
+ * @author AaronFM
+ */
 public class EntityControllersException extends SQLException {
+
+    /**
+     *
+     * @param reason
+     */
     public EntityControllersException(String reason) {
         super(reason);
     }
