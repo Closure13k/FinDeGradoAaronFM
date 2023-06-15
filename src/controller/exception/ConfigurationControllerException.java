36c47423cbb1f1277e07721ffb052ed02e0d2e19
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-15 13:53:16 +0200

    Update ConfigurationControllerException.java

--- src/controller/exception/ConfigurationControllerException.java
+++ src/controller/exception/ConfigurationControllerException.java
@@ -1,17 +1,60 @@
 package controller.exception;
 
+/**
+ * Mensajes de error para el controlador de configuración.
+ * @author AaronFM
+ */
 public class ConfigurationControllerException extends Exception {
+
+    /**
+     *
+     */
     public static final String MISSING = "No se pudo encontrar la propiedad: ";
+
+    /**
+     *
+     */
     public static final String DB_IP = "db.ip: IP de la base de datos.";
+
+    /**
+     *
+     */
     public static final String DB_PORT = "db.port: Puerto de la base de datos.";
+
+    /**
+     *
+     */
     public static final String DB_USER = "db.user: Usuario de la base de datos.";
+
+    /**
+     *
+     */
     public static final String DB_PASSWORD = "db.password: Clave de la base de datos.";
+
+    /**
+     *
+     */
     public static final String FTP_IP = "ftp.ip: IP del servidor FTP.";
+
+    /**
+     *
+     */
     public static final String FTP_PORT = "ftp.port: Puerto del servidor FTP.";
+
+    /**
+     *
+     */
     public static final String FTP_USER = "ftp.user: Usuario del servidor FTP.";
+
+    /**
+     *
+     */
     public static final String FTP_PASSWORD = "ftp.password: Clave del servidor FTP.";
 
-
+    /**
+     *
+     * @param message
+     */
     public ConfigurationControllerException(String message) {
         super(message);
     }
