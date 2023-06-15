5ee2c9c8f07fe074148913c3b63ff91d29062516
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-15 13:53:21 +0200

    Update ImagePickerController.java

--- src/controller/ftp/ImagePickerController.java
+++ src/controller/ftp/ImagePickerController.java
@@ -1,12 +1,18 @@
 package controller.ftp;
 
 import java.io.File;
-import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+/**
+ *
+ * @author Administrador
+ */
 public class ImagePickerController {
-
+    /**
+     * Lanza el imagePicker para su uso.
+     * @return 
+     */
     public static JFileChooser pickImage() {
         JFileChooser jfc = new JFileChooser();
         jfc.setCurrentDirectory(new File(System.getProperty("user.home")));
