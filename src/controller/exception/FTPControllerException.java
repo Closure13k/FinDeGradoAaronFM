b06ac4c401d1ed08c8b76950a47aafcefda92061
Aaron Manuel Fernández <aaronmfm97@hotmail.com>
2023-06-15 13:53:18 +0200

    Update FTPControllerException.java

--- src/controller/exception/FTPControllerException.java
+++ src/controller/exception/FTPControllerException.java
@@ -1,8 +1,16 @@
 package controller.exception;
 
 import java.io.IOException;
-
+/**
+ * Excepciones personalizadas para InputOutput(específicas de FTP).
+ * @author AaronFM
+ */
 public class FTPControllerException extends IOException {
+
+    /**
+     *
+     * @param message
+     */
     public FTPControllerException(String message) {
         super(message);
     }
